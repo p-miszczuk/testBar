d9671bb7c1886993883ecdbb3c943c5695def2c2
prezes2018 <szczuk2@interia.pl>
2019-06-26 21:20:36 +0200

    create listeners functions, set message text and position of bar

--- build/widget.js
+++ build/widget.js
@@ -2,13 +2,33 @@
 
 function Bar(data) {
 
-  // handles
-  var barButton = document.querySelector(".show-bar");
-  var bar = document.querySelector('.bar');
-  var barExit = document.getElementById("bar-exit");
-  var text = document.getElementById('bar-text');
+    // get values
+    this.message = data.message;
+    this.position = data.position;
 
-  // listeners
-  barButton.addEventListener("click", handleShowBar, false);
-  barExit.addEventListener("click", handleHideBar, false);
+    // handles
+    var barButton = document.querySelector(".show-bar");
+    var bar = document.querySelector('.bar');
+    var barExit = document.getElementById("bar-exit");
+    var text = document.getElementById('bar-text');
+
+    //set message and bar's position
+    text.innerHTML = this.message;
+    this.position === "top" ? bar.style.top = "0" : bar.style.bottom = "0";
+
+    // listeners
+    barButton.addEventListener("click", handleShowBar, false);
+    barExit.addEventListener("click", handleHideBar, false);
+
+    //set bar
+    function handleShowBar(e) {
+        e.preventDefault();
+        if (bar.style.display === "none" || bar.style.display == "") bar.style.display = "block";
+    }
+
+    //hide bar
+    function handleHideBar(e) {
+        e.preventDefault();
+        bar.style.display = "none";
+    }
 }
--- src/widget.js
+++ src/widget.js
@@ -1,15 +1,34 @@
   function Bar(data) {
+
+    // get values
+    this.message = data.message;
+    this.position = data.position;
     
     // handles
     const barButton = document.querySelector(".show-bar");
     const bar = document.querySelector('.bar');
     const barExit = document.getElementById("bar-exit");
     const text = document.getElementById('bar-text');
+    
+    //set message and bar's position
+    text.innerHTML = this.message;
+    this.position === "top" ? bar.style.top = "0" : bar.style.bottom = "0";
 
     // listeners
     barButton.addEventListener("click",handleShowBar,false);
     barExit.addEventListener("click",handleHideBar,false);
 
+    //set bar
+    function handleShowBar(e) { 
+      e.preventDefault();
+      if (bar.style.display === "none" || bar.style.display == "")
+        bar.style.display = "block";
+    }
+
+    //hide bar
+    function handleHideBar(e) {
+      e.preventDefault();
+      bar.style.display = "none";
+    }
+
   }
-
-
